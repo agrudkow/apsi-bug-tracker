7d7a090443cc8802b842ea03118404a5a5f8b5d3
Artur Grudkowski <agrudkow@gmail.com>
2021-11-12 00:45:58 +0100

    feat(example-material-ui): add an example of material ui page

--- frontend/src/feature/example_view/example_view.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const ExampleView: React.FC = () => {
-  return <div>Example route</div>;
-};
--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -1,3 +1,3 @@
 export enum Routes {
-  Example = 'example',
+  Paperbase = 'paperbase',
 }
