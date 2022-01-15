8e4a46d872979eef8de962486de28921f6b17d6f
Artur Grudkowski <agrudkow@gmail.com>
2022-01-15 17:01:09 +0100

    refactor: fix forrmating, add error handling

--- lambda/mail_notifications/index.py
+++ lambda/mail_notifications/index.py
@@ -1,31 +1,61 @@
 import json
+import logging
+import os
+
 import boto3
 
-def handler(event, context):
+logging.basicConfig()
+logger = logging.getLogger('mailer_lambda')
+logger.setLevel(int(os.environ['LOG_LEVEL']))
+
+logger.info('Lambda initialized.')
+
+
+def handler(event, _):
+    logger.debug('request: {}'.format(json.dumps(event)))
     values = eval(event["body"])
-    client = boto3.client('ses')
-    response = client.send_email(
-    Source='apsibugtracker@gmail.com',
-    Destination={
-        'ToAddresses': values["recipients"],
-        'CcAddresses': [],
-        'BccAddresses': []
-    },
-    Message={
-        'Subject': {
-            'Data': f'Zgłoszenie {values["issue"]}',
-            'Charset': 'UTF-8'
-        },
-        'Body': {
-            'Text': {
-                'Data': f'Zgłoszenie {values["issue"]} zostało zmodyfikowane.\nZespół BugTracker',
-                'Charset': 'UTF-8',
-            }
+    try:
+        client = boto3.client('ses')
+
+        client.send_email(
+            Source='apsibugtracker@gmail.com',
+            Destination={
+                'ToAddresses': values["recipients"],
+                'CcAddresses': [],
+                'BccAddresses': []
+            },
+            Message={
+                'Subject': {
+                    'Data': f'Zgłoszenie {values["issue"]}',
+                    'Charset': 'UTF-8'
+                },
+                'Body': {
+                    'Text': {
+                        'Data':
+                        f'Zgłoszenie {values["issue"]} zostało zmodyfikowane.\nZespół BugTracker',
+                        'Charset': 'UTF-8',
+                    }
+                }
+            })
+    except KeyError as ex:
+        logger.error(str(ex))
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Content-Type': 'test/plain'
+            },
+            'body': str(ex)
         }
-    }
-)
-    print('request: {}'.format(json.dumps(event)))
-    
+    except Exception as ex:
+        logger.error(str(ex))
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'test/plain'
+            },
+            'body': str(ex)
+        }
+
     return {
         'statusCode': 200,
         'headers': {
