736427e297c38b84b892e5639a992865981e1b03
Małgorzata Wróbel <88003229+malwro@users.noreply.github.com>
2022-02-07 17:12:55 +0100

    fix: uniformize variable names

--- lambda/update_problem/test_lambda.py
+++ lambda/update_problem/test_lambda.py
@@ -33,8 +33,8 @@ if __name__ == '__main__':
                         'xdddd'
                     ]),
                     # 'relatedProblems': '1',
-                    'comment_message': 'Komentarz testowy.',
-                    'comment_message_username': 'Jeff'
+                    'commentMessage': 'Komentarz testowy.',
+                    'commentMessageUsername': 'Jeff'
                 })
         }, 1)
 
