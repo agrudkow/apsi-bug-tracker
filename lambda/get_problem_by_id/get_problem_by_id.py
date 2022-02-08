5086e4fb391476f55aad4894a45337cd6c5e9deb
mwrobel2 <malgorzatawrobel687@gmail.com>
2022-02-08 23:46:45 +0100

    fix: get parent/child report IDs

--- lambda/get_problem_by_id/get_problem_by_id.py
+++ lambda/get_problem_by_id/get_problem_by_id.py
@@ -10,7 +10,8 @@ def get_problem_by_id(id: int):
     output = {}
     with get_db_session() as session:
         report = session.query(Report).get(id)
-        related_bugs = session.query(Bug).filter(Bug.parent_bug_id == report.bug_id).all()
+        related_bugs = session.query(Bug).filter(Bug.parent_bug_id == report.bug_id).all() # children
+        parent_bug = session.query(Bug).filter(Bug.id == report.bug_id).one_or_none() # parent
         related_users = report.related_users
         creator = [related_user for related_user in related_users if related_user.role.description == 'CREATOR'][0]
         responsible_person = [
@@ -21,21 +22,12 @@ def get_problem_by_id(id: int):
         ]
         related_messages = session.query(Message).filter(Message.report_id == report.id)
         gathered_messages = []
-        # one_message = []
-        # for message in related_messages:
-        #     message_dict = {
-        #         "send_date": str(message.send_date),
-        #         "username": message.username,
-        #         "text": message.text
-        #     }
-        #     gathered_messages.append(message_dict)
-
         for message in related_messages:
             one_message = []
             one_message.append(str(message.send_date))
             one_message.append(message.username)
             one_message.append(message.text)
-            one_message = ', '.join(element for element in one_message)
+            one_message = ' | '.join(element for element in one_message)
             gathered_messages.append(one_message)
 
         output["problemID"] = str(report.id)
@@ -49,7 +41,7 @@ def get_problem_by_id(id: int):
         output["version"] = str(report.version)
         output["keywords"] = ', '.join([key_word.text for key_word in report.key_words])
         output["description"] = str(report.bug.description)
-        output["relatedProblems"] = ', '.join([str(bug.report.id) for bug in related_bugs])
+        output["relatedProblems"] = str(parent_bug.parent_bug_id) + "/" + ', '.join([str(bug.report.id) for bug in related_bugs])
         output["proposedDeadline"] = str(report.deadline)
         output["status"] = str(report.status_name)
         output["responsiblePerson"] = str(responsible_person[0].username) if len(responsible_person) == 1 else ''
