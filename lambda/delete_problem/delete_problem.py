2ad0c31b5b0a54c583174e32640d0d52b7963038
mwrobel2 <malgorzatawrobel687@gmail.com>
2022-02-09 13:29:16 +0100

    fix(lambda delete_problem): delete messages related to report

--- lambda/delete_problem/delete_problem.py
+++ lambda/delete_problem/delete_problem.py
@@ -1,5 +1,5 @@
 from apsi_database.database import get_db_session
-from apsi_database.models import Report, Bug, RelatedUser
+from apsi_database.models import Report, Bug, RelatedUser, Message
 
 
 class BugTreeException(Exception):
@@ -15,6 +15,7 @@ def delete_problem(id: int) -> None:
         bug = problem.bug
         related_bugs = session.query(Bug).filter(Bug.parent_bug_id == problem.bug_id).all()
         related_users = session.query(RelatedUser).filter(RelatedUser.report_id == problem.id).all()
+        related_messages = session.query(Message).filter(Message.report_id == problem.id).all()
 
         if len(related_bugs) > 0:
             raise BugTreeException('Problem has child problems and cannot be deleted. Delete child problems first.')
@@ -23,5 +24,8 @@ def delete_problem(id: int) -> None:
             session.delete(user)
         session.delete(problem)
         session.delete(bug)
+
+        for message in related_messages:
+            session.delete(message)
 
         session.commit()
