94701cba662f705e30b8a28f2ced2e710aedf3f2
ktusien <kas20tu@gmail.com>
2022-02-09 16:18:46 +0100

    feat(frontend): new features

--- frontend/src/feature/common/axiosInstance.ts
+++ frontend/src/feature/common/axiosInstance.ts
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const apsi_backend = axios.create({
   // TODO: add url to env variables
   // Add url to your api gateway
-  baseURL: 'https://xlwkn4jyqf.execute-api.eu-central-1.amazonaws.com/prod/',
+  baseURL: 'https://tspi3l3wx5.execute-api.eu-central-1.amazonaws.com/prod/',
 });
